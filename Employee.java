3b2799e8d8a5be1505c63e0a6677720eb246c214
meghana242005 <meghana242005@gmail.com>
2025-05-07 14:03:40 +0530

    save

---
+++ Employee.java
@@ -0,0 +1,25 @@
+package java1;
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+    public Employee(String name,int id,double salary){
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    public void displayInfo(){
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("salary: " +salary);
+    }
+
+    public static void main(String[] args){
+        Employee emp1 = new Employee("Ram" , 101, 50000.0);
+        emp1.displayInfo();
+
+    }
+
+    
+}
